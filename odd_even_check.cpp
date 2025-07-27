ec18a84668e4c75f45715acc4cc0f00cebc770b4
Yashvesh20 <yashvesh20@gmail.com>
2025-07-27 16:14:38 +0530

    Create odd_even_check.cpp

---
+++ odd_even_check.cpp
@@ -0,0 +1,23 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout <<"Enter a number ";
+cin >> n;
+if (n%2==0)
+{
+    cout<<"This is an even number";
+
+}
+else
+{
+    cout<<"This is an Odd number";
+}
+}
+
+output:
+Enter a number 20
+This is an even number
+Enter a number 5
+This is an Odd number
